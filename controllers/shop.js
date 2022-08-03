41cb73c99b9724bab17d4649f902d59ebacdc30a
Sameer Watts <sameerwatts@Sameers-MacBook-Pro.local>
2022-08-04 01:03:38 +0530

    extract dynamic params

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  console.log('prodId', prodId);
+  res.redirect("/");
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchProduct((products) => {
     res.render("shop/index", {
