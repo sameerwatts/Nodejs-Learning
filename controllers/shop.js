e49cbf77d2353f0318b82005d6393bba5b26b0b8
Sameer Watts <sameerwatts@Sameers-MacBook-Pro.local>
2022-08-05 16:40:24 +0530

    add a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,4 @@
+const Cart = require("../models/cart");
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
@@ -12,14 +13,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findProductByID(prodId, product => {
-    res.render('shop/product-detail', {
+  Product.findProductByID(prodId, (product) => {
+    res.render("shop/product-detail", {
       product: product,
       pageTitle: product.pageTitle,
-      path: '/products'
-    })
-  })
-}
+      path: "/products",
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchProduct((products) => {
@@ -32,28 +33,30 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render("shop/cart", {
-      pageTitle: "Your Cart",
-      path: "/cart",
-    })
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
 };
 
 exports.postCart = (req, res, next) => {
-  const {productId} = req.body;
-  console.log(productId);
-  res.redirect("/")
+  const { productId } = req.body;
+  Product.findProductByID(productId, (product) => {
+    Cart.addProduct(productId, product.price);
+  });
+  res.redirect("/");
 };
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     pageTitle: "Your Orders",
     path: "/orders",
-  })
+  });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     pageTitle: "Checkout",
     path: "/checkout",
-  })
+  });
 };
