2848503507de7cdf1f713589eb8357c3db7e3f6f
Sameer <sameer.watts00@gmail.com>
2022-08-16 20:50:03 +0530

    Add a one to man relationship

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set("views", "views");
 const adminRouter = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,8 +24,11 @@ app.use(shopRoutes);
 
 app.use(errorController.getPageNotFound);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({force: true})
   .then((result) => {
     // console.log(result);
     app.listen(3001);
