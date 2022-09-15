ee6c77b590f7851f27cf149270a14c619f28852f
Sameer <sameer.watts00@gmail.com>
2022-09-15 20:48:23 +0530

    Fetch all products

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then((product) => {
       res.render("shop/index", {
         prods: product,
@@ -13,7 +13,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then((product) => {
       res.render("shop/product-list", {
         prods: product,
--- routes/shop.js
+++ routes/shop.js
@@ -8,17 +8,17 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
-router.get("/product/:productId", shopController.getProduct);
+// router.get("/product/:productId", shopController.getProduct);
 
-router.post("/cart", shopController.postCart);
+// router.post("/cart", shopController.postCart);
 
-router.get("/cart", shopController.getCart);
+// router.get("/cart", shopController.getCart);
 
-router.post("/create-order", shopController.postOrder);
+// router.post("/create-order", shopController.postOrder);
 
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+// router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
-router.get("/orders", shopController.getOrders);
+// router.get("/orders", shopController.getOrders);
 
 
 module.exports = router;
